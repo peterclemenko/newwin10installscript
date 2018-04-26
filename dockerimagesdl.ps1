f20c68a43cf540b84f8c1f821ffc62b09d1d66fd
aoighost <aoighost137@gmail.com>
2018-04-26 13:40:25 -0400

    docker images download script, base install for now

---
+++ dockerimagesdl.ps1
@@ -0,0 +1,6 @@
+docker pull neo4j
+docker pull aoighost/neo4j-pluggedin-i
+docker pull node
+docker pull node:9-stretch
+docker pull nodered/node-red-docker
+docker pull aoighost/node-stretch-powershell-i
